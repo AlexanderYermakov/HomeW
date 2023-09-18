feb9763ef820c0ca159ab03076f5ca07d940a041
Alejandro Yermakov <cakkoc@yandex.ru>
2023-09-19 02:30:15 +0500

    HW 2 corrected

--- Home_works/Home_work_2/main.py
+++ Home_works/Home_work_2/main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
+# Домашняя работа к семинару 2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Task 1
+# Напишите программу, которая получает целое число и
+# возвращает его шестнадцатеричное строковое представление.
+# Функцию hex используйте для проверки своего результата.
+
+# basis_number = int(input("Введите число для перевода: "))
+# trans_values = {'10':'a', '11':'b', '12':'c', '13':'d', '14':'e', '15':'f'}
+# list_values = [10, 11, 12, 13, 14, 15]
+# change_index = 16
+
+# def Trans_number(number, change_index):
+#     result = ' '
+#     while number:
+#         current = number % change_index
+#         if list_values.count(current) != 0:
+#             current = trans_values.get(str(current))
+#         result = f'{current}' + result
+#         number //= change_index
+#     return result
+#
+# print(f'Число {basis_number} в шестнадцатиричной системе - {Trans_number(basis_number, change_index)}')
+# print(f'Проверочный результат - {hex(basis_number)}')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Task2
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import fractions
+
+numerator1, denominator1 = map(int, input('Введите первую дробь без пробелов используя знак "/" - ').split('/'))
+numerator2, denominator2 = map(int, input('Введите вторую дробь без пробелов используя знак "/" - ').split('/'))
+# получение числителя и знаменателя дробей
+
+def Frac_multiplication(num1, denom1, num2, denom2):
+    return f'{num1 * num2}/{denom1 * denom2}'
+
+def Frac_Summation(num1, denom1, num2, denom2):
+    if denom1 == denom2:
+        return f'{num1 + num2}/{denom1}'
+    else:
+        return 'Жесть'
+
+mult = fractions.Fraction(numerator1, denominator1) * fractions.Fraction(numerator2, denominator2)
+sum = fractions.Fraction(numerator1, denominator1) + fractions.Fraction(numerator2, denominator2)
+
+print(Frac_multiplication(numerator1, denominator1, numerator2, denominator2))
+print(mult)
+
+print(Frac_Summation(numerator1, denominator1, numerator2, denominator2))
+print(sum)

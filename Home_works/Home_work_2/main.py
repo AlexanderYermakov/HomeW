be50f9772432a55718f3e4df4d4e0fcd727a693a
Alejandro Yermakov <cakkoc@yandex.ru>
2023-09-20 01:18:27 +0500

    HW 2 complete

--- Home_works/Home_work_2/main.py
+++ Home_works/Home_work_2/main.py
@@ -5,23 +5,27 @@
 # возвращает его шестнадцатеричное строковое представление.
 # Функцию hex используйте для проверки своего результата.
 
-# basis_number = int(input("Введите число для перевода: "))
-# trans_values = {'10':'a', '11':'b', '12':'c', '13':'d', '14':'e', '15':'f'}
-# list_values = [10, 11, 12, 13, 14, 15]
-# change_index = 16
+def Trans_number_hex(number):
 
-# def Trans_number(number, change_index):
-#     result = ' '
-#     while number:
-#         current = number % change_index
-#         if list_values.count(current) != 0:
-#             current = trans_values.get(str(current))
-#         result = f'{current}' + result
-#         number //= change_index
-#     return result
-#
-# print(f'Число {basis_number} в шестнадцатиричной системе - {Trans_number(basis_number, change_index)}')
-# print(f'Проверочный результат - {hex(basis_number)}')
+    change_index = 16
+    trans_values = {'10': 'a', '11': 'b', '12': 'c', '13': 'd', '14': 'e', '15': 'f'}
+    list_values = [10, 11, 12, 13, 14, 15]
+    result = ' '
+
+    while number:
+        current = number % change_index
+
+        if list_values.count(current) != 0:
+            current = trans_values.get(str(current))
+        result = f'{current}' + result
+        number //= change_index
+
+    return result
+
+basis_number = int(input("Введите число для перевода: "))
+
+print(f'Число {basis_number} в шестнадцатиричной системе - {Trans_number_hex(basis_number)}')
+print(f'Проверочный результат - {hex(basis_number)}')
 
 
 
@@ -31,25 +35,27 @@
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 
 import fractions
+def Frac_multiplication(num1, denom1, num2, denom2):
+    result = f'{num1 * num2}/{denom1 * denom2}'
+    return result
+
+def Frac_summation(num1, denom1, num2, denom2):
+    if denom1 == denom2:
+        result = f'{num1 + num2}/{denom1}'
+        return result
+    else:
+        result = f'{num1 * denom2 + num2 * denom1}/{denom1 * denom2}'
+        return result
 
 numerator1, denominator1 = map(int, input('Введите первую дробь без пробелов используя знак "/" - ').split('/'))
 numerator2, denominator2 = map(int, input('Введите вторую дробь без пробелов используя знак "/" - ').split('/'))
 # получение числителя и знаменателя дробей
 
-def Frac_multiplication(num1, denom1, num2, denom2):
-    return f'{num1 * num2}/{denom1 * denom2}'
+multiplication_check = fractions.Fraction(numerator1, denominator1) * fractions.Fraction(numerator2, denominator2)
+sum_check = fractions.Fraction(numerator1, denominator1) + fractions.Fraction(numerator2, denominator2)
 
-def Frac_Summation(num1, denom1, num2, denom2):
-    if denom1 == denom2:
-        return f'{num1 + num2}/{denom1}'
-    else:
-        return 'Жесть'
+print(f'Произведение данных дробей: {Frac_multiplication(numerator1, denominator1, numerator2, denominator2)}')
+print(f'Проверочный результат произведения: {multiplication_check}')
 
-mult = fractions.Fraction(numerator1, denominator1) * fractions.Fraction(numerator2, denominator2)
-sum = fractions.Fraction(numerator1, denominator1) + fractions.Fraction(numerator2, denominator2)
-
-print(Frac_multiplication(numerator1, denominator1, numerator2, denominator2))
-print(mult)
-
-print(Frac_Summation(numerator1, denominator1, numerator2, denominator2))
-print(sum)
+print(f'Сумма данных дробей: {Frac_summation(numerator1, denominator1, numerator2, denominator2)}')
+print(f'Проверочный результат суммы: {sum_check}')
